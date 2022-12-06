5f562bc1c07487849f412484b09a1bd5beb30c2a
mr-rjh3 <mrrjh3@gmail.com>
2022-12-05 22:21:29 -0500

    Added middle line, ball turning red based on speed, Match point and winner text

--- game.cpp
+++ game.cpp
@@ -8,7 +8,7 @@
 using namespace std;
 
 void Game::ballMove() {
-    if(!scored && !paused) {
+    if(!scored && !paused && score[0] < MATCH_POINT && score[0] < MATCH_POINT){
         ball.x += ball.xSpeed;
         ball.y += ball.ySpeed;
     }
@@ -21,7 +21,7 @@ void Game::checkCollision() {
             // Bounce ball
             if(ball.xSpeed > 0){ // only bounce if the ball is moving towards the paddle to prevent multiple bounces
                 if(abs(ball.xSpeed) < SPEED_CAP) { // Caps the speed 
-                    ball.xSpeed *= -1.05; // increase the speed by 5% and invert the direction
+                    ball.xSpeed *= -1.1; // increase the speed by 5% and invert the direction
                 }
                 else { // if the speed is capped
                     ball.xSpeed = -ball.xSpeed; // just invert the direction
@@ -37,7 +37,7 @@ void Game::checkCollision() {
             // Bounce ball
             if(ball.xSpeed < 0){ // only bounce if the ball is moving towards the paddle to prevent multiple bounces
                 if(abs(ball.xSpeed) < SPEED_CAP) { // Caps the speed
-                    ball.xSpeed *= -1.05; // increase the speed by 5% and invert the direction
+                    ball.xSpeed *= -1.1; // increase the speed by 5% and invert the direction
                 }
                 else{ // if the speed is capped
                     ball.xSpeed = -ball.xSpeed; // just invert the direction
@@ -59,35 +59,33 @@ void Game::checkCollision() {
     // right wall 
     if(ball.x + ball.radius >= 800) { 
         scorePoint();
-        PlaySound(TEXT("sounds/pointScored.wav"), NULL, SND_FILENAME | SND_ASYNC);
     }
     // left wall
     if(ball.x - ball.radius <= 0) { 
         scorePoint();
-        PlaySound(TEXT("sounds/pointScored.wav"), NULL, SND_FILENAME | SND_ASYNC);
     }
 }
 
 void Game::paddleLeftMoveUp(){
-    if(!paused && paddleLeft.y - paddleLeft.height> 0){
+    if(!paused && paddleLeft.y - paddleLeft.height> 0 && score[0] < MATCH_POINT && score[1] < MATCH_POINT){
         paddleLeft.y -= paddleLeft.speed;
         scored = false;
     }
 }
 void Game::paddleLeftMoveDown(){
-    if(!paused && paddleLeft.y + paddleLeft.height < 600){
+    if(!paused && paddleLeft.y + paddleLeft.height < 600 && score[0] < MATCH_POINT && score[1] < MATCH_POINT){
         paddleLeft.y += paddleLeft.speed;
         scored = false;
     }
 }
 void Game::paddleRightMoveUp(){
-    if(!paused && paddleRight.y - paddleRight.height > 0){
+    if(!paused && paddleRight.y - paddleRight.height > 0 && score[0] < MATCH_POINT && score[1] < MATCH_POINT){
         paddleRight.y -= paddleRight.speed;
         scored = false;
     }
 }
 void Game::paddleRightMoveDown(){
-    if(!paused && paddleRight.y + paddleRight.height < 600){
+    if(!paused && paddleRight.y + paddleRight.height < 600 && score[0] < MATCH_POINT && score[1] < MATCH_POINT){
         paddleRight.y += paddleRight.speed;
         scored = false;
     }
@@ -107,34 +105,45 @@ void Game::resetGame() {
     paused = false;
 }
 void Game::scorePoint() {
-        scored = true;
-        if(ball.x + ball.radius >= 800) { // right wall 
-            score[0]++;
-        }
-        if(ball.x - ball.radius <= 0) { // left wall
-            score[1]++;
-        }
-        if(ball.xSpeed > 0) {
-            ball.xSpeed = -2;
+    scored = true;
+    if(ball.x + ball.radius >= 800) { // right wall 
+        score[0]++;
+        ball.xSpeed = 2;
+        if(score[0] == MATCH_POINT) {
+            PlaySound(TEXT("sounds/win.wav"), NULL, SND_FILENAME | SND_ASYNC);            
         }
         else {
-            ball.xSpeed = 2;
+            PlaySound(TEXT("sounds/pointScored.wav"), NULL, SND_FILENAME | SND_ASYNC);
         }
-        ball.ySpeed = 2;
-        ball.x = 400;
-        ball.y = 300;
-        paddleLeft.y = 300;
-        paddleRight.y = 300;
-        cout<<"Score: "<<score[0]<<" - "<<score[1]<<endl;
+    }
+    if(ball.x - ball.radius <= 0) { // left wall
+        score[1]++;
+        ball.xSpeed = -2;
+        if(score[1] == MATCH_POINT) {
+            PlaySound(TEXT("sounds/win.wav"), NULL, SND_FILENAME | SND_ASYNC);
+        }
+        else{
+            PlaySound(TEXT("sounds/pointScored.wav"), NULL, SND_FILENAME | SND_ASYNC);
+        }
+    }
+    // randomize the ySpeed to be either -2 or 2
+    ball.ySpeed = (rand() % 2) * 4 - 2;
+
+    // set the ball and paddles to the center of the screen
+    ball.x = 400;
+    ball.y = 300;
+    paddleLeft.y = 300;
+    paddleRight.y = 300;
+    cout<<"Score: "<<score[0]<<" - "<<score[1]<<endl;
 }
 void Game::pauseGame() {
-    paused = !paused;
-    if(paused) {
-        cout<<"Game Paused"<<endl;
-        PlaySound(TEXT("sounds/pause.wav"), NULL, SND_FILENAME | SND_ASYNC);
-    }
-    else {
-        cout<<"Game Resumed"<<endl;
-        PlaySound(TEXT("sounds/resume.wav"), NULL, SND_FILENAME | SND_ASYNC);
+    if(!scored) {
+        paused = !paused;
+        if(paused) {
+            PlaySound(TEXT("sounds/pause.wav"), NULL, SND_FILENAME | SND_ASYNC);
+        }
+        else {
+            PlaySound(TEXT("sounds/unpause.wav"), NULL, SND_FILENAME | SND_ASYNC);
+        }
     }
 }
